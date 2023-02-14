1141a37a67b79a75215c5c43564c9952b1acbfb0
petan <petan@NISHA>
2023-02-14 17:28:55 +0530

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenClosePrinciple
+{//without implementing open-close principle
+    /*internal class Invoice
+      {
+    public double GetInvoiceDiscount(double amount,Invoice Type invoice Type)
+    {
+    double finalAmount=0*/
+      
+    public class Invoice
+    {
+        public virtual double GetInvoiceAmount(double amount)
+        {
+            return amount - 10;
+
+        }
+    }
+    public class FinalInvoice : Invoice
+    {
+        public override double GetInvoiceAmount(double amount)
+        {
+            
+           
+                return base.GetInvoiceAmount(amount) - 50;
+            }
+        }
+    public class ProposedInvoice : Invoice
+    {
+        public override double GetInvoiceAmount(double amount)
+        {
+            return base.GetInvoiceAmount(amount) - 40;
+        }
+    }
+    }
+
+}
